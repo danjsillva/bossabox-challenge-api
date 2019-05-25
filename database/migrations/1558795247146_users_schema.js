f4e3ea5874b3316a5c20f01155d308daa17bf6d2
Daniel Silva <danielsilva@Daniels-MacBook-Air.local>
2019-05-25 11:52:14 -0300

    Configurado docker e conexao bd

--- database/migrations/1558795247146_users_schema.js
+++ database/migrations/1558795247146_users_schema.js
@@ -3,14 +3,14 @@
 /** @type {import('@adonisjs/lucid/src/Schema')} */
 const Schema = use('Schema')
 
-class UserSchema extends Schema {
+class UsersSchema extends Schema {
   up () {
     this.create('users', (table) => {
       table.increments()
+      table.timestamps()
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
-      table.timestamps()
     })
   }
 
@@ -19,4 +19,4 @@ class UserSchema extends Schema {
   }
 }
 
-module.exports = UserSchema
+module.exports = UsersSchema
